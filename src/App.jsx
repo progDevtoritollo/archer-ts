b1edb3899f0e76140fe36ce4d8cd79eb9ed8db11
Ihor <progdevtoritollo@gmail.com>
2022-05-22 01:02:39 +0300

    fix isAuth hin App

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { Route, Redirect, Switch } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+
+import { Auth, Home } from "./pages";
+
+function App() {
+  const [page, setPage] = useState(false);
+  const { isAuth } = useSelector(({ user }) => user);
+
+  // const isAuth = false;
+
+  useEffect(() => {
+    // console.log(page, isAuth);
+    setPage(isAuth);
+  }, [isAuth, page]);
+
+  // return (
+  //   <div className="wrapper">
+  //     <Switch>
+  //       <Route
+  //         path="/"
+  //         render={() => (isAuth ? <Home /> : <Redirect to="/signin" />)}
+  //       />
+  //       <Redirect to="/signin" />
+  //       <Route
+  //         exact
+  //         path={["/signin", "/signup", "/signup/verify"]}
+  //         component={Auth}
+  //       />
+  //     </Switch>
+  //   </div>
+  // );
+
+  if (page) {
+    return (
+      <div className="wrapper">
+        <Switch>
+          <Route
+            path="/"
+            render={() => (isAuth ? <Home /> : <Redirect to="/signin" />)}
+          />
+        </Switch>
+      </div>
+    );
+  } else {
+    return (
+      <div className="wrapper">
+        <Switch>
+          <Route
+            exact
+            path={["/signin", "/signup", "/signup/verify"]}
+            component={Auth}
+          />
+          <Redirect to="/signin" />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
