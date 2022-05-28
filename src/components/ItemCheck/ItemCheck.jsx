96571a93c52c736d453237b858304bf032b465f7
Ihor <progdevtoritollo@gmail.com>
2022-05-29 01:41:54 +0300

    create ItemCheck

---
+++ src/components/ItemCheck/ItemCheck.jsx
@@ -0,0 +1,13 @@
+import "./ItemCheck.scss";
+
+const ItemCheck = () => (
+  <div className="check">
+    <div className="check__user-name"></div>
+    <div className="chack__date"></div>
+    <div className="check__date"></div>
+    <div className="check__total"></div>
+    <div className="check__distance"></div>
+  </div>
+);
+
+export default ItemCheck;
