2661884c894b74fd069f4c0d85c086d92ce8a73a
progDevtoritollo <progDevtoritollo@gmail.com>
2022-06-02 20:14:19 +0300

    add to home Routs ItemUser

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-import { FooterBar } from "../../components";
+import { FooterBar, ItemUser } from "../../components";
 
 import {
   ProfileNotification,
@@ -21,21 +21,26 @@ const Home = () => {
   return (
     <>
       <div className="home">
-        <Route exact path="/profile/settings" component={ProfileSettings} />
-        <Route
-          exact
-          path="/profile/notification"
-          component={ProfileNotification}
-        />
-        <Route exact path="/profile/statistic" component={ProfileStatistic} />
-        <Route exact path="/profile/checks" component={ProfileChecks} />
+        <Switch>
+          <Route exact path="/profile/settings" component={ProfileSettings} />
+          <Route
+            exact
+            path="/profile/notification"
+            component={ProfileNotification}
+          />
+          <Route exact path="/profile/statistic" component={ProfileStatistic} />
+          <Route exact path="/profile/checks" component={ProfileChecks} />
 
-        <Route exact path="/club/statistic" component={ClubStatistic} />
-        <Route exact path="/club/activity" component={ClubActivity} />
-        <Route exact path="/club/events" component={ClubEvents} />
-        <Route exact path="/club/settings" component={ClubSettings} />
-
-        <Route path="/target" component={TrainingEvent} />
+          <Route exact path="/club/statistic" component={ClubStatistic} />
+          <Route exact path="/club/activity" component={ClubActivity} />
+          <Route exact path="/club/events" component={ClubEvents} />
+          <Route exact path="/club/settings" component={ClubSettings} />
+          <Route path="/user/:id" component={ItemUser} />
+          <Route path="/target" component={TrainingEvent} />
+          <Route path="/404">
+            <h1>404 - Not Found</h1>
+          </Route>
+        </Switch>
       </div>
       <div className="fix-footer"></div>
       <FooterBar />
