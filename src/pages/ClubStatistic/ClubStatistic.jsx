80a6ba225b79929d3f2471e89fc38182be812696
progDevtoritollo <progDevtoritollo@gmail.com>
2022-06-15 19:17:19 +0300

    redering list of members in ClubStatistic

--- src/pages/ClubStatistic/ClubStatistic.jsx
+++ src/pages/ClubStatistic/ClubStatistic.jsx
@@ -3,44 +3,54 @@ import "./ClubStatistic.scss";
 import userService from "../../services/userService";
 import { setUserPage } from "./../../redux/actions/user";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Skeleton, Card, Switch, Avatar, Radio } from "antd";
 import { EllipsisOutlined, ThunderboltOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import clubService from "../../services/clubService";
 const { Meta } = Card;
 
-const ClubStatistic = () => {
-  const dispath = useDispatch();
-
-  const [pageSwitch, setPageSwitch] = useState(true);
-  const users = [
-    {
-      name: "Игорь Марусич",
-      rank: "КМС",
-      id: 54,
-      src: "link_avatar",
-    },
-    { name: "Игорь Марусич", rank: "КМС", id: 45, src: "link_avatar" },
-    { name: "Игорь Марусич", rank: "КМС", id: 1561, src: "link_avatar" },
-  ];
-  const oneUser = {
+const userArray = [
+  {
     name: "Игорь Марусич",
     rank: "КМС",
-    id: 45,
+    id: 54,
     src: "link_avatar",
-  };
+  },
+  { name: "Игорь Марусич", rank: "КМС", id: 45, src: "link_avatar" },
+  { name: "Игорь Марусич", rank: "КМС", id: 1561, src: "link_avatar" },
+];
+const ClubStatistic = () => {
+  const dispath = useDispatch();
+  const [userArray, setUserArray] = useState([]);
+
+  const [pageSwitch, setPageSwitch] = useState(true);
 
   const [loading, setLoading] = useState(true);
 
   const handleUserClick = (id) => {
-    userService.getUserProfile(id);
-    dispath(setUserPage(oneUser));
+    userService.getUserProfile(id).then((res) => {
+      console.log(res.data);
+      dispath(setUserPage(res.data));
+    });
   };
 
-  const onChange = (checked) => {
-    setLoading(!checked);
+  const getData = () => {
+    clubService
+      .getClubMembers()
+      .then((res) => {
+        setUserArray(res.data);
+        setLoading(!loading);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   };
+  useEffect(() => {
+    getData();
+    console.log(userArray);
+  }, []);
 
   return (
     <div className="club-statistic">
@@ -57,7 +67,7 @@ const ClubStatistic = () => {
       </Radio.Group>
 
       {pageSwitch ? (
-        <div>
+        <>
           <h1>Statistic</h1>
 
           <span>Графики и все таке</span>
@@ -67,7 +77,7 @@ const ClubStatistic = () => {
             которая у пользователя - кадет например), в виде графиков топ 3
             лучника,
           </div>
-        </div>
+        </>
       ) : (
         <div>
           <h1>Members</h1>
@@ -78,41 +88,47 @@ const ClubStatistic = () => {
             нажатии, возможностсь вызова на дуэль, статистика кратакая (роост
             или падение в соответствии с прошлой контрольной или тренировкой )
           </span>
-          <div className="members-list">
-            <h1 className="members-list__title">ClubMembers</h1>
-            Список пользователей клуба
-            <Switch checked={!loading} onChange={onChange} />
-            <div className="members-list__container">
-              {users.map((user) => (
-                <Link to={`/user/${user.id}`} key={user.id + "link"}>
-                  <Card
-                    onClick={() => handleUserClick(user.id)}
-                    key={user.id}
-                    className="members-list__item"
-                    style={{
-                      width: "100%",
-                      marginTop: 12,
-                    }}
-                    actions={[
-                      <ThunderboltOutlined key="+" />, // вызвать на дуэль
-                      <EllipsisOutlined key="ellipsis" />, // открыть параментры пользователя
-                    ]}
+          {userArray ? (
+            <div className="members-list">
+              <h1 className="members-list__title">ClubMembers</h1>
+              Список пользователей клуба
+              <div className="members-list__container">
+                {userArray.map((item) => (
+                  <Link
+                    to={`/user/${item.member.user.id}`}
+                    key={item.member.user.id + "link"}
                   >
-                    <Skeleton loading={loading} avatar active>
-                      <Meta
-                        avatar={
-                          <Avatar src="https://joeschmoe.io/api/v1/random" />
-                        }
-                        title={user.name}
-                        description={"Rank: " + user.rank}
-                      />
-                    </Skeleton>
-                    <div>Total this mounth: 235</div>
-                  </Card>
-                </Link>
-              ))}
+                    <Card
+                      onClick={() => handleUserClick(item.member.user.id)}
+                      key={item.member.user.id}
+                      className="members-list__item"
+                      style={{
+                        width: "100%",
+                        marginTop: 12,
+                      }}
+                      actions={[
+                        <ThunderboltOutlined key="+" />, // вызвать на дуэль
+                        <EllipsisOutlined key="ellipsis" />, // открыть параментры пользователя
+                      ]}
+                    >
+                      <Skeleton loading={loading} avatar active>
+                        <Meta
+                          avatar={
+                            <Avatar src="https://joeschmoe.io/api/v1/random" />
+                          }
+                          title={` Name:   ${item.member.user.name}  ${item.member.user.surname}`}
+                          description={`Rank:  ${item.member.user.rank}      
+                              Total this mounth: ${item.lastCheckTotal}`}
+                        />
+                      </Skeleton>
+                    </Card>
+                  </Link>
+                ))}
+              </div>
             </div>
-          </div>
+          ) : (
+            <h1>Нет участников</h1>
+          )}
         </div>
       )}
     </div>
