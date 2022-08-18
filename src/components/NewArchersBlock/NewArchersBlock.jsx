162bec04871ccbb860e656dd5e48ae6ace09d1fa
progDevtoritollo <progDevtoritollo@gmail.com>
2022-08-19 02:50:01 +0300

    add Card to NewArcher Item

---
+++ src/components/NewArchersBlock/NewArchersBlock.jsx
@@ -0,0 +1,37 @@
+import "./NewArchersBlock.scss";
+import clubService from "../../services/clubService";
+import { Card } from "./../index";
+
+import { useState, useEffect } from "react";
+import { Button } from "antd";
+
+const NewArchersBlock = (newArcher) => {
+  const handleAddNewComersClick = (userId) => {
+    clubService.postNewComerAdd(userId);
+    // getNewComersList();
+  };
+
+  return (
+    <div className="archer-block-wrapper">
+      <Card className="archer-card">
+        <div className="archer-card__name">{`Имя:  ${newArcher.name}  `}</div>
+        <div className="archer-card__surename">{`Фамилия: ${newArcher.surname}  `}</div>
+        <div className="archer-card__email">
+          {` Email: ${newArcher.email} `}
+        </div>
+        <div className="archer-card__messageData">
+          {`Дата: ${newArcher.createDtm} `}
+        </div>
+        <Button
+          type="primary"
+          onClick={() => handleAddNewComersClick(newArcher.id)}
+          block
+        >
+          Добавить
+        </Button>
+      </Card>
+    </div>
+  );
+};
+
+export default NewArchersBlock;
--- src/pages/CoachNewarchers/CoachNewarchers.jsx
+++ src/pages/CoachNewarchers/CoachNewarchers.jsx
@@ -1,12 +1,33 @@
-import { NewarchersList } from "../../components";
+import { NewArchersBlock } from "../../components/index";
+import clubService from "../../services/clubService";
 import "./CoachNewarchers.scss";
 
+import { useState, useEffect } from "react";
+
 const CoachNewarchers = () => {
+  const [newComersArray, setNewComersArray] = useState([]);
+
+  const getNewComersList = () => {
+    clubService.getNewComers().then((res) => {
+      setNewComersArray(res.data);
+    });
+    console.log(newComersArray);
+  };
+
+  useEffect(() => {
+    getNewComersList();
+  }, []);
+
+  const items = newComersArray.map((obj) => (
+    <NewArchersBlock key={obj.id} {...obj} />
+  ));
+
   return (
-    <div className="">
-      <h1>CoachNewarchers</h1>
-      <span></span>
-      <NewarchersList />
+    <div className="container">
+      <h1 className="content__top">CoachNewarchers</h1>
+      <div className="content__items">
+        {newComersArray === [] ? <div>Нет новых участников</div> : items}
+      </div>
     </div>
   );
 };
