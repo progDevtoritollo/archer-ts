8e349aacc041eaa2d700ed57e54e56beeca230cd
Igor Marusych <progdevtoritollo@gmail.com>
2022-10-29 01:25:58 +0300

    config connect to backend API dev && prod

--- src/commons.js
+++ src/commons.js
@@ -1,3 +1,6 @@
-export const API = window.location.origin + "/api";
+// export const API = window.location.origin + "/api";    // for prod
 
-export const GOOGLE_API = API + "/oauth2/authorize/google";
+export const SERVER_LOCATION = "http://localhost:8080"; //for dev
+export const API = SERVER_LOCATION + "/api"; //for dev
+
+export const GOOGLE_API = SERVER_LOCATION + "/oauth2/authorize/google";
