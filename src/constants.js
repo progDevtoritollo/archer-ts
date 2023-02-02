a25aa5d25c7edaba414e4799cae683aea4367496
Igor Marusych <progdevtoritollo@gmail.com>
2023-02-02 11:26:11 +0200

    add const ranks

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,11 @@
 export const ARCHERS_RANKS = {
-  U1: { name: "Юноша 1 ", color: "magenta" },
-  U2: { name: "Юноша 2 ранг", color: "red" },
-  U3: { name: "Юноша 3 ранг", color: "volcano" },
+  U1: { name: "Юноша 1", color: "magenta" },
+  U2: { name: "Юноша 2", color: "red" },
+  U3: { name: "Юноша 3", color: "volcano" },
 
-  A1: { name: "Взрос 1 ранг", color: "orange" },
-  A2: { name: "Взрос 2 ранг", color: "gold" },
-  A3: { name: "Взрос 3 ранг", color: "lime" },
+  A1: { name: "Взрос 1", color: "orange" },
+  A2: { name: "Взрос 2", color: "gold" },
+  A3: { name: "Взрос 3", color: "lime" },
 
   KMC: { name: "KMC", color: "green" },
   MC: { name: "MC", color: "blue" },
